595dceb8ad785dadf8e19a4431b0d48b8d645ed0
ilya <ilya.selfrealization@gmail.com>
2020-01-22 23:55:06 +0300

    add zuul

---
+++ zuul-server/src/main/java/com/ere/services/zuulservice/config/ZuulOwnFilter.java
@@ -0,0 +1,41 @@
+package com.ere.services.zuulservice.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Zuul filter
+ *
+ * @author ilya
+ * @version 1.0
+ */
+@Component
+public class ZuulOwnFilter extends ZuulFilter {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        var request = RequestContext.getCurrentContext().getRequest();
+        logger.info("request: " + request.getRequestURI());
+        return null;
+    }
+}
