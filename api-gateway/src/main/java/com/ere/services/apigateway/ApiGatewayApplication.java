07f1bde257da21add860f619bf544d2b2276b12f
ilya <ilya.selfrealization@gmail.com>
2020-01-28 23:47:51 +0300

    init reactive gateway service

---
+++ api-gateway/src/main/java/com/ere/services/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.ere.services.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("car-service", r -> r.path("/orders")
+						.uri("lb://car-service"))
+				.build();
+	}
+
+}
---
+++ api-gateway/src/test/java/com/ere/services/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ere.services.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
