0f3be9bc9dba5fcdb79dd41f0538fb9afe00bf83
ilya <ilya.selfrealization@gmail.com>
2020-02-06 19:57:41 +0300

    init config service and add account service

--- api-gateway/src/main/java/com/ere/services/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ere/services/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,6 @@ package com.ere.services.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
---
+++ configuration-service/README.md
@@ -0,0 +1,14 @@
+# configuration-service
+Simple cloud configuration server for my own services
+
+This server-service works with my local git repo.
+I have *.properties files in work package for MongoDB, PostreSQL and ets.
+
+This server you can use like an example.
+
+
+More information about cloud:
+https://cloud.spring.io/spring-cloud-config/reference/html/
+
+Useful link:
+https://spring.io/guides/gs/centralized-configuration/
