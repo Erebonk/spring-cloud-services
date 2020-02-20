f594fa1383a10fd7f568a6c1c9922cfe454b0114
ilya <ilya.selfrealization@gmail.com>
2020-02-20 19:02:52 +0300

    add atype-catelog api for products

---
+++ atype-catalog/src/test/java/com/services/catalog/atypecatalog/AtypeCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.catalog.atypecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtypeCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
