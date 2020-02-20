19c748e2747b3d4cc3e52e3eeacbcd5838c87457
ilya <ilya.selfrealization@gmail.com>
2020-02-20 19:02:11 +0300

    add hystrix load balancer

--- searcher/readme.md
+++ searcher/readme.md
@@ -1,4 +1,3 @@
 Hystrix stream:
 
 localhost:8076/actuator/hystrix.stream
-
--- searcher/src/main/java/com/ere/services/searcher/service/ClientHystrixService.java
+++ searcher/src/main/java/com/ere/services/searcher/service/ClientHystrixService.java
@@ -1,9 +1,17 @@
 package com.ere.services.searcher.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.springframework.stereotype.Service;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
-@Service
+@Configuration
 public class ClientHystrixService {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
