3da76068b65a74133c81b661c64eb5b51661538b
ilya <ilya.selfrealization@gmail.com>
2020-03-02 22:53:56 +0300

    add hystrix command

---
+++ catalog-api/src/main/java/com/ere/catalogapi/service/AtypeProxyService.java
@@ -0,0 +1,21 @@
+package com.ere.catalogapi.service;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "atype-catalog")
+@RibbonClient(name = "atype-catalog")
+public interface AtypeProxyService {
+
+    @GetMapping("catalog/types")
+    Object getPriceTypes();
+
+    @GetMapping("catalog/category")
+    Object getCategories(@RequestParam("priceType") String priceType);
+
+    @GetMapping("catalog/product")
+    Object getProducts(@RequestParam("priceType") String priceType, @RequestParam("categoryId") String categoryId);
+
+}
